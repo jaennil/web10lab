921c3ba7aebb4b40acb2aa88d55e242646ec4ef1
jaennil <jaennil@yandex.ru>
2023-11-29 19:46:07 +0300

    feat: complete

--- result.php
+++ result.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
@@ -14,42 +15,53 @@
         function test_symbols($text)
         {
             $symbols = array();
-            $lower_text = strtolower($text);
-            for ($i = 0; $i < strlen($lower_text); $i++) {
-                if (isset($symbols[$lower_text[$i]])) {
-                    $symbols[$lower_text[$i]]++;
+            $lower_text = mb_strtolower($text, 'utf-8');
+            for ($i = 0; $i < mb_strlen($lower_text, 'utf-8'); $i++) {
+                $char = mb_substr($lower_text, $i, 1, 'utf-8');
+                if (isset($symbols[$char])) {
+                    $symbols[$char]++;
                 } else {
-                    $symbols[$lower_text[$i]] = 1;
+                    $symbols[$char] = 1;
                 }
             }
+            ksort($symbols);
             return $symbols;
         }
 
         function test_it($text)
         {
-            echo 'Количество символов: ' . strlen($text) . '<br>';
-            $digits = array(
-                '0' => true,
-                '1' => true,
-                '2' => true,
-                '3' => true,
-                '4' => true,
-                '5' => true,
-                '6' => true,
-                '7' => true,
-                '8' => true,
-                '9' => true
-            );
+            $eng_alph = 'abcdefghijklmnopqrstuvwxyz';
+            $rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+            $punct_marks = ".,?!-_:;'[]()“”«»";
+            $digits = "0123456789";
             $digits_amount = 0;
-            $words_amount = 0;
+            $letters_amount = 0;
+            $punct_marks_amount = 0;
+            $upper_letters = 0;
+            $lower_letters = 0;
             $current_word = '';
             $words = array();
-            for ($i = 0; $i < strlen($text); $i++) {
-                if (array_key_exists($text[$i], $digits)) {
+            for ($i = 0; $i < mb_strlen($text, 'utf-8'); $i++) {
+                $char = mb_substr($text, $i, 1, 'utf-8');
+                if (str_contains($digits, $char)) {
                     $digits_amount++;
                 }
 
-                if ($text[$i] == ' ' || $i == strlen($text) - 1) {
+                if (str_contains($rus_alph . $eng_alph, $char)) {
+                    $letters_amount++;
+                    $lower_letters++;
+                }
+
+                if (str_contains(mb_strtoupper($rus_alph, 'utf-8') . mb_strtoupper($eng_alph, 'utf-8'), $char)) {
+                    $letters_amount++;
+                    $upper_letters++;
+                }
+
+                if (str_contains($punct_marks, $char)) {
+                    $punct_marks_amount++;
+                }
+
+                if ($char == ' ' || str_contains($punct_marks, $char) || $i == mb_strlen($text, 'utf-8') - 1) {
                     if ($current_word) {
                         if (isset($words[$current_word])) {
                             $words[$current_word]++;
@@ -59,20 +71,38 @@
                     }
                     $current_word = '';
                 } else {
-                    $current_word .= $text[$i];
+                    $current_word .= $char;
                 }
             }
-            echo 'Количество цифр: ' . $digits_amount . '<br>';
-            echo 'Количество слов: ' . count($words) . '<br>';
+            echo '1. количество символов в тексте (включая пробелы): ' . mb_strlen($text, 'utf-8') . '<br>';
+            echo '2. количество букв: ' . $letters_amount . '<br>';
+            echo "3. количество строчных и заглавных букв по отдельности<br>";
+            echo '- строчных: ' . $lower_letters . '<br>';
+            echo '- заглавных: ' . $upper_letters . '<br>';
+            echo '4. количество знаков препинания: ' . $punct_marks_amount . '<br>';
+            echo '5. количество цифр: ' . $digits_amount . '<br>';
+            echo '6. количество слов: ' . count($words) . '<br>';
+            echo '7. количество вхождений каждого символа текста:<br>';
+            $symbols = test_symbols($text);
+            foreach ($symbols as $key => $value) {
+                echo "Символ '" . $key . "': " . $value . " шт.<br>";
+            }
+            echo '8. Список всех слов в тексте и количество их вхождений, отсортированный по алфавиту:<br>';
+            ksort($words);
+            foreach ($words as $word => $count) {
+                echo "   Слово '" . htmlspecialchars($word) . "': " . $count . " шт.<br>";
+            }
         }
 
-        if (isset($_POST["data"]) && $_POST["data"]) {
-            echo '<div class="src_text">' . $_POST["data"] . "</div>";
+        $text = $_POST["data"];
+        if (isset($_POST["data"]) && strlen(trim($_POST['data']))) {
+            echo '<div class="src_text">' . $text . "</div>";
             test_it($_POST["data"]);
         } else {
             echo '<div class="src_error">Нет текста для анализа</div>';
         }
         ?>
+        <a href="/index.html">Другой анализ</a>
     </main>
 </body>
 
